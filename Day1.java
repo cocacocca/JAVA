21443444d3cd51cec1f36d9d2e2c8b423b08be22
cocacocca <915917282@qq.com>
2023-07-29 21:10:26 +0800

    1

---
+++ Day1.java
@@ -0,0 +1,13 @@
+public class Day1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello! world!");
+        //单行注释
+
+        /*
+        *多行注释
+        *
+        */
+    }
+}
